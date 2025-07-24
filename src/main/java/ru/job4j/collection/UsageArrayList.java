5fc70d257c455019d2cd404cdf9c4b38410a8306
aleks <girbov@gmail.com>
2025-07-24 23:15:40 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Petr");
+        stringList.add("Ivan");
+        stringList.add("Stepan");
+
+        stringList.forEach(System.out::println);
+    }
+}
